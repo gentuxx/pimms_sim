f81eb332804652f3a7fa2f10843bb887c1e34b9f
gentuxx <loic.martinux@gmail.com>
2024-08-29 11:11:09 +0200

    Questionnaire autocomplete five stars

--- saisie_questionnaire.js
+++ saisie_questionnaire.js
@@ -32,20 +32,6 @@
 
             if (combobox.value.trim() == "5 étoiles") {
 
-                /*
-                document.querySelector("#name").value = "INDIVIDUEL";
-                document.querySelector("#place").value = "NARBONNE";
-                document.querySelector("#nb_duration_hour").value = 90;
-                document.querySelector("#nb_mediator").value = 1;
-                document.querySelector("#nb_attendee").value = 1;
-
-                $("#operator_id").val(293);
-                $("#operator_id").change();
-
-                $("#operator_action_id").val(1393);
-                $("#operator_action_id").change();
-                */
-
                 checkRadioButton("q0_rate5");
                 checkRadioButton("q1_rate5");
                 checkRadioButton("q2_rate5");
@@ -53,21 +39,7 @@
                 checkRadioButton("q4_rate5");
             }
         }
-
         divTwo.appendChild(combobox);
-
-        /*
-        document.querySelector("#name").value = "INDIVIDUEL";
-        document.querySelector("#place").value = "NARBONNE";
-        document.querySelector("#nb_duration_hour").value = 90;
-        document.querySelector("#nb_mediator").value = 1;
-        document.querySelector("#nb_attendee").value = 1;
-
-        $("#operator_id").val(293);
-        $("#operator_id").change();
-
-        $("#operator_action_id").val(1393);
-        $("#operator_action_id").change();*/
     }
 
     function checkRadioButton(name) {
