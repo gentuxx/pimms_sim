6e8bfd7cd3469654928f28d84e08dd263819c84a
gentuxx <loic.martinux@gmail.com>
2024-08-30 02:12:21 +0200

    Automatization

--- saisie_questionnaire.js
+++ saisie_questionnaire.js
@@ -19,70 +19,61 @@
         return;
     }
 
+    var MAX_VALUES_TO_ENTER = 5;
+    var MIN_TIME = 15;
+    var MAX_TIME = 30;
+
+    var index = 0;
+
     const structure = document.getElementById("structure_selected").textContent.trim();
 
     if (structure == "Narbonne" || structure == "Sigean Corbières Méditerranée")
     {
-        /*
         const demoClasses = document.querySelectorAll('.card-text');
+
+        var buttonSave = document.getElementsByClassName("btn")[1];
+
+        buttonSave.addEventListener("click", (event) => {loadValues()});
 
         var divEl = demoClasses[0];
         divEl.insertAdjacentHTML('afterbegin', '<div id="div_two" style="margin-bottom: 15px" align="left"></div>');
 
         var divTwo = document.getElementById("div_two");
-        var combobox = document.createElement("select");
+        var myButton = document.createElement("button");
+        myButton.innerHTML = "Automatisation";
 
-        combobox.options[0] = new Option("5 étoiles", "5 étoiles");
+        var interval;
 
-        Array.from(combobox.options).forEach((element) => (element.onclick = loadValues));
+        myButton.addEventListener("click", function() {
+            setDelay();
+        });
 
-        function loadValues() {
+        function setDelay() {
 
-            if (combobox.value.trim() == "5 étoiles") {
+            clearInterval(interval);
 
-                uncheckRadioButton("q0_rate1");
-                uncheckRadioButton("q0_rate2");
-                uncheckRadioButton("q0_rate3");
-                uncheckRadioButton("q0_rate4");
-                uncheckRadioButton("q0_rate5");
+            var rand = getRandomInt(MIN_TIME, MAX_TIME);
 
-                uncheckRadioButton("q1_rate1");
-                uncheckRadioButton("q1_rate2");
-                uncheckRadioButton("q1_rate3");
-                uncheckRadioButton("q1_rate4");
-                uncheckRadioButton("q1_rate5");
+            loadValues();
+            buttonSave.click();
 
-                uncheckRadioButton("q2_rate1");
-                uncheckRadioButton("q2_rate2");
-                uncheckRadioButton("q2_rate3");
-                uncheckRadioButton("q2_rate4");
-                uncheckRadioButton("q2_rate5");
+            interval = setInterval(setDelay, rand * 1000);
 
-                uncheckRadioButton("q3_rate1");
-                uncheckRadioButton("q3_rate2");
-                uncheckRadioButton("q3_rate3");
-                uncheckRadioButton("q3_rate4");
-                uncheckRadioButton("q3_rate5");
+            ++index;
 
-                uncheckRadioButton("q4_rate1");
-                uncheckRadioButton("q4_rate2");
-                uncheckRadioButton("q4_rate3");
-                uncheckRadioButton("q4_rate4");
-                uncheckRadioButton("q4_rate5");
-
-                checkRadioButton("q0_rate5");
-                checkRadioButton("q1_rate5");
-                checkRadioButton("q2_rate5");
-                checkRadioButton("q3_rate5");
-                checkRadioButton("q4_rate5");
+            if (index >= MAX_VALUES_TO_ENTER) {
+                clearInterval(interval);
+                return;
             }
         }
-        divTwo.appendChild(combobox);
-    }*/
 
-        var button = document.getElementsByClassName("btn")[1];
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-        button.addEventListener("click", (event) => {loadValues()});
+        divTwo.appendChild(myButton);
 
         function loadValues() {
 
