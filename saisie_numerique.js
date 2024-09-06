7bdc6ac1272a1fa465288c0c27a566e2fc986275
gentuxx <loic.martinux@gmail.com>
2024-09-06 09:12:13 +0200

    Addibng Sigean site

--- saisie_numerique.js
+++ saisie_numerique.js
@@ -12,7 +12,11 @@
 (function() {
     'use strict';
 
-    if (document.getElementById("structure_selected").textContent.trim() == 'Narbonne - Pand@')
+    var structure = document.getElementById("structure_selected").textContent.trim();
+    var bNarbonne = structure == 'Narbonne - Pand@';
+    var bSigean = structure == "Sigean Corbières Méditerranée";
+
+    if (bNarbonne || bSigean)
     {
         const demoClasses = document.querySelectorAll('.card-title');
 
@@ -21,14 +25,35 @@
 
         var divTwo = document.getElementById("div_two");
         var combobox = document.createElement("select");
+        combobox.setAttribute("id", "combobox");
 
-        combobox.options[0] = new Option('Séance 1h30', 'Séance 1h30');
-        combobox.options[1] = new Option('Pré-séance', 'Pré-séance');
-        combobox.options[2] = new Option('Collectif', 'Collectif');
+        if (bNarbonne) {
+            combobox.options[0] = new Option('Séance 1h30', 'Séance 1h30');
+            combobox.options[1] = new Option('Pré-séance', 'Pré-séance');
+            combobox.options[2] = new Option('Collectif', 'Collectif');
+        }
+        else if (bSigean) {
+            combobox.options[0] = new Option('Séance 1h', 'Séance 1h');
+        }
 
-        Array.from(combobox.options).forEach((element) => (element.onclick = loadValues));
+        loadValues();
+
+        divTwo.appendChild(combobox);
+
+        loadValues();
+
+        combobox.selectedIndex = 0;
+        combobox[0].dispatchEvent(new Event("click"));
 
         function loadValues() {
+            if (bNarbonne) {
+                Array.from(combobox.options).forEach((element) => (element.onclick = loadValuesNarbonne));
+            } else if (bSigean) {
+                Array.from(combobox.options).forEach((element) => (element.onclick = loadValuesSigean));
+            }
+        }
+
+        function loadValuesNarbonne() {
             if (combobox.value.trim() == 'Séance 1h30') {
                 document.querySelector("#name").value = "INDIVIDUEL";
                 document.querySelector("#place").value = "NARBONNE";
@@ -70,18 +95,20 @@
             }
         }
 
-        divTwo.appendChild(combobox);
+        function loadValuesSigean() {
+            if (combobox.value.trim() == 'Séance 1h') {
+                document.querySelector("#name").value = "INDIVIDUEL";
+                document.querySelector("#place").value = "SIGEAN";
+                document.querySelector("#nb_duration_hour").value = 60;
+                document.querySelector("#nb_mediator").value = 1;
+                document.querySelector("#nb_attendee").value = 1;
 
-        document.querySelector("#name").value = "INDIVIDUEL";
-        document.querySelector("#place").value = "NARBONNE";
-        document.querySelector("#nb_duration_hour").value = 90;
-        document.querySelector("#nb_mediator").value = 1;
-        document.querySelector("#nb_attendee").value = 1;
+                $("#operator_id").val(293);
+                $("#operator_id").change();
 
-        $("#operator_id").val(293);
-        $("#operator_id").change();
-
-        $("#operator_action_id").val(1393);
-        $("#operator_action_id").change();
+                $("#operator_action_id").val(1393);
+                $("#operator_action_id").change();
+            }
+        }
     }
 })();
